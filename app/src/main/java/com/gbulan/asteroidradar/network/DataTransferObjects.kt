cfed1f8e0c55461f605f80ea806e015f5d79ce6b
gulsahbulan <gulsah.bulan@gmail.com>
2021-04-28 20:05:47 -0700

    refactor: Remove unused codes

--- app/src/main/java/com/gbulan/asteroidradar/network/DataTransferObjects.kt
+++ app/src/main/java/com/gbulan/asteroidradar/network/DataTransferObjects.kt
@@ -1,6 +1,5 @@
 package com.gbulan.asteroidradar.network
 
-import com.gbulan.asteroidradar.domain.Asteroid
 import com.gbulan.asteroidradar.database.DatabaseAsteroid
 import com.squareup.moshi.JsonClass
 
@@ -14,23 +13,6 @@ data class NetworkAsteroid(
         val relativeVelocity: Double,
         val distanceFromEarth: Double,
         val isPotentiallyHazardous: Boolean)
-
-/**
- * Convert Network results to database objects
- */
-fun List<NetworkAsteroid>.asDomainModel(): List<Asteroid> {
-    return map {
-        Asteroid(
-                id = it.id,
-                codename = it.codename,
-                closeApproachDate = it.closeApproachDate,
-                absoluteMagnitude = it.absoluteMagnitude,
-                estimatedDiameter = it.estimatedDiameter,
-                relativeVelocity = it.relativeVelocity,
-                distanceFromEarth = it.distanceFromEarth,
-                isPotentiallyHazardous = it.isPotentiallyHazardous)
-    }
-}
 
 fun List<NetworkAsteroid>.asDatabaseModel(): List<DatabaseAsteroid> {
     return map {
